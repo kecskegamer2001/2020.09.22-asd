f753b90f5f64a98f9c22986b23caef4eaac5be76
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-09-22 09:00:27 +0200

    Add project files.

---
+++ 2020.09.22 asd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020._09._22_asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("van Mansom Peter");
+            Console.ReadKey();
+
+        }
+    }
+}
